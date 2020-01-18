5bd32cef9c39da79d4876d94f867e221f8b78188
Ryan Anderson <shnoutyanderson@gmail.com>
2020-01-18 00:30:32 -0800

    am now generating a QR code...a completely BS one though.

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 
+var initialized = false; // this boolean flag is used so we don't trigger the intialize function twice.
+
 $.get("https://ipinfo.io", function(response) { 
-    initialize(response.ip); 
+    if(!initialized) initialize(response.ip); 
 }, "json") ;
 
 
 function initialize(ip) {
+
     console.log(ip);
     $('#my-qr').html(ip);
+
+    new QRCode(document.getElementById("my-qr"), "http://jindo.dev.naver.com/collie");
 }
